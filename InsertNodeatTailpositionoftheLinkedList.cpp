569fed2b36e7d53dc4e871cacb09d9ad312ab143
rahuljha12527 <45179877+rahuljha12527@users.noreply.github.com>
2019-06-11 10:12:41 +0530

    Add files via upload

---
+++ InsertNodeatTailpositionoftheLinkedList.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+        }
+
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the insertNodeAtTail function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtTail(SinglyLinkedListNode* head, int data) {
+    SinglyLinkedListNode * new_node=new SinglyLinkedListNode(data);
+    if(head==NULL){
+      head=new_node;
+      return head;
+    }
+
+    SinglyLinkedListNode *current_node=head;
+    while(current_node->next!=NULL){
+      current_node=current_node->next;
+    }
+
+    current_node->next=new_node;
+    return head;
+
+
+
+}
+
+int main()
